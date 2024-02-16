f1ad8f9c9a79dc57ff41940890941dd371a556ec
Terje Nesthus, working from home <terje@fdvhuset.no>
2024-02-16 20:40:41 +0100

    Created first page

--- app/Filament/Pages/Settings.php
+++ app/Filament/Pages/Settings.php
@@ -8,7 +8,13 @@ class Settings extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
+//    protected static string $view = 'settings.main';
     protected static string $view = 'filament.pages.settings';
+
+    protected static ?string $title = 'Settings';
+    protected static ?string $navigationLabel = 'Custom Navigation Label';
+    protected ?string $heading = 'Custom Page Heading';
+    protected ?string $subheading = 'Custom Page Subheading';
 
     public static function canAccess(): bool
     {
---
+++ resources/views/settings/main.blade.php
@@ -0,0 +1,5 @@
+{{-- filament/settings/custom-page.blade.php --}}
+<div>
+    <h1>This is my custom Settings page content</h1>
+    {{-- Add your custom settings fields, forms, etc. here --}}
+</div>
